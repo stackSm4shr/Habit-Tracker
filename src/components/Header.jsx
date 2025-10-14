3a4f4d95d3c30c282a60e4d488b93ffff7969a2b
Tim <timbecker1988@gmail.com>
2025-10-14 17:38:13 +0200

    basic_function_and_styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="flex flex-col items-center mb-8">
+      <h1 className="text-4xl font-bold mb-2">HabitSpark</h1>
+      <p className="text-xl">“Make Every Checkmark Count.”</p>
+    </div>
+  );
+}
