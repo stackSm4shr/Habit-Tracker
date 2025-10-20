e076352933c004bd99992f33371b49287e57fca6
Tim <timbecker1988@gmail.com>
2025-10-20 09:59:28 +0200

    add counter to localstorage

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -2,7 +2,7 @@ import { HabitListItem } from "./HabitListItem";
 
 export function HabitList({ habits, setHabits }) {
   return (
-    <div className="w-full max-w-md bg-white border rounded ">
+    <div className="w-full max-w-md bg-white border rounded">
       <h2 className="text-2xl font-semibold p-4 border-b">Habit List</h2>
       {habits.length === 0 ? (
         <p className="p-4 text-gray-500">No habits yet. Add one above!</p>
